d2ae4e0d48846bc285e82f4e522e3f497f8dd455
Reid Taylor <reidhtaylor@icloud.com>
2023-12-16 18:37:43 -0500

    Initial Commit

---
+++ AM/AMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMApp.swift
+//  AM
+//
+//  Created by Reid Taylor on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AM
+//
+//  Created by Reid Taylor on 12/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
